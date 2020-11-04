c84412df09358191505e56b33ba7eb8a5259c817
yag-dev <53407061+yag-dev-xda@users.noreply.github.com>
2020-11-04 18:56:38 +0000

    Initial 4.0 Release

---
+++ FlashFinished.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TrebleToolkit4
+{
+    /// <summary>
+    /// Interaction logic for FlashFinished.xaml
+    /// </summary>
+    public partial class FlashFinished : Page
+    {
+        public FlashFinished()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Uri uri = new Uri("HomePage.xaml", UriKind.Relative);
+            this.NavigationService.Navigate(uri);
+        }
+    }
+}
---
+++ FlashGSI.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TrebleToolkit4
+{
+    /// <summary>
+    /// Interaction logic for FlashGSI.xaml
+    /// </summary>
+    public partial class FlashGSI : Page
+    {
+        public FlashGSI()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Uri uri = new Uri("HomePage.xaml", UriKind.Relative);
+            this.NavigationService.Navigate(uri);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Uri uri = new Uri("GSIFlashAB.xaml", UriKind.Relative);
+            this.NavigationService.Navigate(uri);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Uri uri = new Uri("GSIFlashA.xaml", UriKind.Relative);
+            this.NavigationService.Navigate(uri);
+        }
+    }
+}
