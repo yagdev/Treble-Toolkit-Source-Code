effff33d374c622057fdbfde89dbc716af62a569
yag-dev <53407061+yag-dev-xda@users.noreply.github.com>
2020-11-25 21:18:38 +0000

    Update 5.0.0.0

---
+++ TrebleToolkit4/About.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TrebleToolkit4
+{
+    /// <summary>
+    /// Interaction logic for About.xaml
+    /// </summary>
+    public partial class About : Window
+    {
+        public About()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C start updater.exe";
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+    }
+}
---
+++ TrebleToolkit4/UnlockBL.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TrebleToolkit4
+{
+    /// <summary>
+    /// Interaction logic for UnlockBL.xaml
+    /// </summary>
+    public partial class UnlockBL : Page
+    {
+        public UnlockBL()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            const string strCmdText = "/C cd pt & start adb.exe reboot-bootloader & start fastboot.exe oem unlock";
+            Process.Start("CMD.exe", strCmdText);
+            Uri uri = new Uri("BLUnlocked.xaml", UriKind.Relative);
+            this.NavigationService.Navigate(uri);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Uri uri = new Uri("HomePage.xaml", UriKind.Relative);
+            this.NavigationService.Navigate(uri);
+        }
+    }
+}
